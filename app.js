c99e4da8e89d78d930bc9cf0eea5d9195de2ce04
Tushar Sekhri <TusharSekhri@MacBook-Air-2.local>
2022-06-04 20:53:18 -0400

    Development complete, One server

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
 var express = require('express');
 var app = express();
+var dict = new Map();
+var famousComics = [];
+var cors = require("cors");
+const bodyParser = require('body-parser')
+const path = require("path")
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "./build")));
+app.listen(5000, function () {
+  console.log('App listening on port 5000!');
+});
+app.get("*", (req, res) => {
+  res.sendfile(path.join(__dirname + "/build/index.html"))});
+
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(5000, function () {
-  console.log('Example app listening on port 3000!');
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+
+
+app.post('/comic', function (req, res) {
+
+  var comicID = req.body.comicID;
+  if (dict.has(comicID)) {
+    dict.set(comicID, dict.get(comicID) + 1);
+  } else {
+    dict.set(comicID, 1);
+  }
+  var ind = famousComics.findIndex(element => element[0] == comicID);
+  console.log(comicID, famousComics, ind);
+  if (ind == -1) {
+    famousComics.push([comicID, dict.get(comicID)]);
+  } else {
+    famousComics[ind][1] += 1;
+  }
+  console.log(dict);
+  famousComics.sort(function(a, b) {
+    if (a[1] > b[1]) return -1;
+    if (a[1] < b[1]) return 1;
+    return 0;
+  });
+  if (famousComics.length > 5) {
+    famousComics.pop();
+  }
+
+  res.send({famousComics: famousComics});
 });
--- frontend/src/Comic.js
+++ frontend/src/Comic.js
@@ -15,52 +15,149 @@ export class Comic extends Component {
         
         let comicID = parseInt(this.props.match.params.id);
         this.state = {
-            
             comicID: comicID,
-            img: null
+            img: null,
+            date: null,
+            transcript: null,
+            famousComics: null
         };
+        this.maxComicID = comicID;
     }
 
     comicViewed = async function(comicID) {
         axios
             .get('https://xkcd.com/' + comicID + '/info.0.json')
             .then(res => {  
-                this.setState({img: res.data.img});
+                this.setState({img: res.data.img, date: res.data.day + '/' + res.data.month + '/' + res.data.year, transcript: res.data.transcript});
             });
     };
+/*
+    async componentDidMount() {
+        // find Max
+        this.comicViewed(this.state.comicID);
+        axios
+            .get('https://xkcd.com/info.0.json')
+            .then(res => {  
+                this.setState({maxComicID: parseInt(res.data.num)}, async function() {
+                    console.log("11")
+                    console.log(this.state)
+                    if (isNaN(this.state.comicID)) {
+                        console.log("12")
+                        this.setState({comicID: this.state.comicID}, async function() {
+                            this.comicViewed(this.state.comicID);
 
+                        })
+                    } else {
+                        this.comicViewed(this.state.comicID);
+                    }
+                });
+        });
+    }
+*/
 
+/*
+    findMax = async function() {
+        axios
+            .get('https://xkcd.com/info.0.json')
+            .then(res => {
+                this.maxComicID = parseInt(res.data.num)
+                if (isNaN(this.state.comicID)) {
+                    this.state.comicID = this.maxComicID
+                }
+        });
+    }
+*/
 
     async componentDidMount() {
-    
-        if (isNaN(this.state.comicID)) {
-            axios
-                .get('https://xkcd.com/info.0.json')
-                .then(res => {  
-                    this.setState({maxComicID: parseInt(res.data.num)});
-            });
-        }
-        this.comicViewed(this.state.comicID);
+        // find Max
+        console.log("11")
+        await axios
+            .get('https://xkcd.com/info.0.json')
+            .then(res => {  
+                console.log("12")
+                this.maxComicID = parseInt(res.data.num)
+                console.log(this.maxComicID);
+                });
 
+        if (isNaN(this.state.comicID) || (this.state.comicID < 1) || (this.state.comicID > this.maxComicID)) {
+            window.location.href='/' + this.maxComicID
+        } else {
+            await axios
+                .post('http://localhost:5000/comic', {comicID: this.state.comicID})
+                .then(res => {
+                    console.log(res.data.famousComics)  
+                    this.setState({famousComics: res.data.famousComics})
+                    });
+            this.comicViewed(this.state.comicID)
+        }
     }
 
+/*
     componentDidUpdate(prevProps, prevState) {
         if (prevState.comicID !== this.state.comicID) {
-            if (isNaN(this.state.comicID)) {
-                this.setState({comicID: this.state.maxComicID});
-            }
             this.comicViewed(this.state.comicID);
         }
     }
 
+
+                            for(let i = 0; i < this.state.famousComics.length, i++) {
+                            <tr> <th>{this.state.famousComics[i][0]}</th> <th>{this.state.famousComics[i][1]}</th> </tr>
+                        }
+*/
+    
+
     render() {
+        let displayTranscript = () => {
+            if (this.state.transcript) {
+                let splitTranscript = this.state.transcript.split("\n");      
+                return <div className="transcript">  {splitTranscript.map(function(line, i) {
+                            if (! line.startsWith("{{")) {
+                                return <p key={i}> {line} </p>;
+                            }
+                    })} </div>
+            }
+        }
+
+        let famousComics = () => {
+            if (this.state.famousComics) {  
+                return <div className="famousComics">  
+                    <h4> Most famous comics </h4>
+                {
+                    <table>
+                        <thead>
+                        <tr><th>Id</th><th>Count</th></tr>
+                        </thead>
+                        <tbody>
+                    { 
+                        this.state.famousComics.map(function(item, i) {
+                            return <tr key={i}><td> {item[0]} </td><td> {item[1]} </td></tr>;
+                        })
+                    }
+                        </tbody>
+                    </table>
+                } </div>
+            }
+        }
+        
         return ( 
         <div>
-            <h1> {this.state.comicID} </h1>
+            <h2> Comic ID: {this.state.comicID} </h2>
+            <h3> Date created: {this.state.date} </h3>
             <img src={this.state.img}/>
-            <br />
-            <button type="button" onClick={event =>  window.location.href='/' + (this.state.comicID - 1)}>Prev</button>
-            <button type="button" onClick={event =>  window.location.href='/' + (this.state.comicID + 1)}>Next</button>    
+            { displayTranscript() }
+            { famousComics() }
+            <div className="buttonInlineDiv">
+            <div className="buttonDiv buttonInline">
+            <button   type="button" onClick={event =>  window.location.href='/' + (this.state.comicID - 1)}>Prev</button>
+            </div>
+            <div  className="buttonDiv buttonInline">
+            <button   type="button" onClick={event =>  window.location.href='/' + (this.state.comicID + 1)}>Next</button>    
+            </div>
+            </div>
+            
+            <div className="buttonDiv">
+            <button  type="button" onClick={event =>  window.location.href='/' + (Math.floor(Math.random() * this.maxComicID))}>Random</button>    
+            </div>
         </div>
         );
     }
